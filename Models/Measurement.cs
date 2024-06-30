33fe587c7bacf2221bcb3bba36141fbb13b64514
Akseli Närhi <timo.narhi@ajrsolutions.fi>
2024-06-30 19:08:34 +0300

    Updated Measurement.cs model

--- Models/Measurement.cs
+++ Models/Measurement.cs
@@ -1,9 +1,13 @@
-using System.Data.Entity;
-
-
-
-    public class MeasurementDbContext : DbContext
+namespace Measurement.Models
+{
+    public class Measurement
     {
-        public DbSet<Measurement> Measurements {get; set;}
+        public int Id {get; set;}
+        public string Name {get; set;} 
+        public float Temperature {get; set;} 
+        public float Humidity {get; set;}
+        public string Mac {get; set;}
+        public int MovementCounter {get; set;}
+        public DateTime Date {get; set;}
     }
 }
